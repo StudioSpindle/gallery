ee67ac79189289c43b47d4578bd6e96911dfd209
Vledder <remi@studiospindle.nl>
2019-08-01 10:55:40 +0200

    add loader

--- src/assets/js/entry.js
+++ src/assets/js/entry.js
@@ -3,14 +3,19 @@ import generateCardHTML from './modules/generateCardHTML';
 import getGalleryPhotos from './modules/getGalleryPhotos';
 
 const galleryDomNode = document.getElementById('js-gallery');
+const loader = document.getElementById('js-loading');
 
 getGalleryPhotos(apiUrlGallery, apiUrlPhoto)
-  .then(data => generateCardHTML(data, galleryDomNode))
+  .then((data) => {
+    generateCardHTML(data, galleryDomNode);
+    galleryDomNode.removeChild(loader);
+  })
   .catch((rej) => {
     const errorFeedback = document.createElement('section');
+    galleryDomNode.removeChild(loader);
     errorFeedback.innerHTML = `
-      <h2>An error occured</h2>
-      <p>Reason: ${rej}</p>
+      <h2>An error occured.</h2>
+      <p>${rej}</p>
     `;
     galleryDomNode.appendChild(errorFeedback);
   });
