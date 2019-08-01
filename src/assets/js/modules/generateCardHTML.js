4247a9753969ee6a696af6d90a818fa0b9b381ce
Vledder <remi@studiospindle.nl>
2019-08-01 10:56:57 +0200

    fix promise rejection if image size is not available

--- src/assets/js/modules/generateCardHTML.js
+++ src/assets/js/modules/generateCardHTML.js
@@ -5,24 +5,47 @@
 export default function generateCardHTML(data, destNode) {
   data.forEach((photo) => {
     // image aspect ratio is by default 4:3
-    const photoSrcSmall = photo.sizes.size[4];
-    // const photoSrcMedium = photo.sizes.size[5];
-    // const photoSrcLarge = photo.sizes.size[7];
+    const photoSrcSmall = photo.sizes.size[4] ? photo.sizes.size[4] : null;
+    const photoSrcMedium = photo.sizes.size[5] ? photo.sizes.size[5] : null;
+    const photoSrcLarge = photo.sizes.size[7] ? photo.sizes.size[7] : null;
 
     const template = document.createElement('template');
     if (photo.stat !== 'ok') {
       template.innerHTML = `
-        <div class="card">
+        <div class="card gallery__item">
           <p>Something is wrong with this photo</h2>
         </div>
       `;
     } else {
+      let srcSet;
+      let sizes;
+      if (photoSrcSmall) {
+        srcSet = `${photoSrcSmall.source} 240w`;
+        sizes = '(max-width: 600px) 320px';
+      }
+      if (photoSrcMedium) {
+        srcSet = `${photoSrcMedium.source} 320w`;
+        sizes = '(min-width: 600px) 320px';
+      }
+      // for some images the large size is not available
+      if (photoSrcLarge) {
+        srcSet = `${photoSrcMedium.source} 720w`;
+        sizes = ', (min-width: 900px) 720px';
+      }
+      // browsers that don't have srcset will fallback on src,
+      //  therefore the medium size is used
       template.innerHTML = `
-        <div class="card">
-          <div>
-            <img class="card__image" width="${photoSrcSmall.width}" height="${photoSrcSmall.height}" src="${photoSrcSmall.source}" alt="Image named ${photo.title}" />
+        <div class="card gallery__item">
+          <img 
+            class="card__image"     
+            alt="Image named ${photo.title}" 
+            src="${photoSrcMedium.source}"
+            srcset="${srcSet}"
+            sizes="${sizes}"
+          />
+          <div class="card__title">
+            <h2 class="serif">${photo.title}</h2>
           </div>
-          <h2>${photo.title}</h2>
         </div>
       `;
     }
--- src/assets/js/modules/getGalleryPhotos.js
+++ src/assets/js/modules/getGalleryPhotos.js
@@ -1,16 +1,25 @@
 /**
+ * @param {string} url The url to perform the fetch on
+ */
+async function getJSON(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
  * @param {string} urlGallery The URL to fetch the gallery from
  * @param {string} urlPhoto The URL to fetch the photo meta information from
  */
 export default async function getGalleryPhotos(urlGallery, urlPhoto) {
-  const galleryPhotoResponse = await fetch(urlGallery);
-  const galleryPhotoResponseJSON = await galleryPhotoResponse.json();
-
+  const galleryPhotoResponseJSON = await getJSON(urlGallery);
   const photos = galleryPhotoResponseJSON.photos.photo.map(async (photo) => {
     const { title } = photo;
     const photoUrl = `${urlPhoto}&photo_id=${photo.id}`;
-    const photoResponse = await fetch(photoUrl);
-    const photoResponseJSON = await photoResponse.json();
+    const photoResponseJSON = await getJSON(photoUrl);
     return { ...photoResponseJSON, title };
   });
   return Promise.all(photos);
